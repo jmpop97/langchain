eb3e86d8e24740d5479a85afa1f8ff79a6b19c8f
jmpop97 <pjm970128@gmail.com>
2025-03-20 14:30:00 +0900

    add -llm

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import ollama
 import time
 import json
+import numpy as np
+from html.parser import HTMLParser
+from langchain_community.embeddings import OllamaEmbeddings
 
-
-def sample():
-    filename = 'prompt.txt'
+def sample(filename):
     f = open(filename, 'r')     # mode = 부분은 생략해도 됨
         # 결과 사이 경계선
     lines = f.read()
@@ -32,7 +33,7 @@ def send(model,content):
     end_time = time.time()
     elapsed_time=end_time - start_time
     print("elapsed time - ", elapsed_time, "seconds")
-    jsonData=        {
+    jsonData= {
             "start_time":start_time,
             "end_time":end_time,
             "elapsed_time":elapsed_time,
@@ -40,29 +41,88 @@ def send(model,content):
         }
     return jsonData
 
+def modelsTest():
+    models=[
+            'gemma3',
+            'qwq',
+            'deepseek-r1',
+            'llama3.3',
+            'phi4',
+            'nomic-embed-text',
+            'mistral',
+            'qwen2.5',
+            'gemma',
+            'codellama',
+            'neural-chat',
+            'starling-lm',
+            'solar'
+            ]
+    file='prompt.txt'
+    err=[]
+    for model in models:
+        try:
+            text=sample()
+            jsonData=send(model,text)
+            saveJson(model,jsonData)
+        except:
+            err.append(model)
+    saveJson("err",err)
+    print(err)
 
-models=[
-        'gemma3',
-        'qwq',
-        'deepseek-r1',
-        'llama3.3',
-        'phi4',
-        'nomic-embed-text',
-        'mistral',
-        'qwen2.5',
-        'gemma',
-        'codellama',
-        'neural-chat',
-        'starling-lm',
-        'solar'
-        ]
-err=[]
-for model in models:
-    try:
-        text=sample()
-        jsonData=send(model,text)
-        saveJson(model,jsonData)
-    except:
-        err.append(model)
-saveJson("err",err)
-print(err)
+
+
+
+class MyHTMLParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.result = []
+
+    def handle_starttag(self, tag, attrs):
+        self.result.append(f"<{tag}>")
+
+    def handle_endtag(self, tag):
+        self.result.append(f"</{tag}>")
+
+    def handle_data(self, data):
+        if data.strip():
+            self.result.append(data.strip())
+
+def split_html(html):
+    parser = MyHTMLParser()
+    parser.feed(html)
+    return parser.result
+
+def llm():
+    chunckedSize=10
+    chunckedRangeSize=2
+    question="선예매 회당 몇매할 수 있어? 숫자로 알려줘."
+    file="promptSample.txt"
+
+    text=sample(file)
+    testList=split_html(text)
+    testList=["".join(testList[i:i+chunckedSize]) for i in range(0, len(testList), chunckedSize)]
+    testListRange = ["".join(testList[i:i+chunckedRangeSize]) for i in range(len(testList)-chunckedRangeSize+1)]
+
+
+    ollama_embeddings = OllamaEmbeddings(
+        model="nomic-embed-text",
+        # model="chatfire/bge-m3:q8_0" # BGE-M3
+    )
+    embedded_documents = ollama_embeddings.embed_documents(testListRange)
+
+    embedded_query = ollama_embeddings.embed_query(question)
+
+
+    # 질문(embedded_query): LangChain 에 대해서 알려주세요.
+    similarity = np.array(embedded_query) @ np.array(embedded_documents).T
+
+    # 유사도 기준 내림차순 정렬
+    sorted_idx = (np.array(embedded_query) @ np.array(embedded_documents).T).argsort()[::-1]
+
+    # 결과 출력
+    print(question)
+    for i, idx in enumerate(sorted_idx):
+        if i<3:
+            print(f"[{i}] 유사도: {similarity[idx]:.3f} | {testList[idx]}")
+            print()
+llm()
